3d0d66b4cad9be390db93ed1f39681f9dcc01608
NotSnowWhite <gflatch@att.net>
2024-05-09 13:09:11 -0500

    filled out handlebars with data and created public js files with data

---
+++ public/js/logout.js
@@ -0,0 +1,15 @@
+const logout = async () => {
+    const response = await fetch('/logout', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+    });
+  
+    if (response.ok) {
+      document.location.replace('/');
+    } else {
+      alert('Problem logging out.');
+    }
+  };
+  
+  document.querySelector('.logout-btn').addEventListener('click', logout);
+  
