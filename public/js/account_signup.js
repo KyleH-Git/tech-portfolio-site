560c13ee42badf8804d81fe17237404fd8234ae8
NotSnowWhite <gflatch@att.net>
2024-05-10 12:25:37 -0500

    updated profile and signup handlebars and js

--- public/js/account_signup.js
+++ public/js/account_signup.js
@@ -1,29 +1,16 @@
-// creating a widget to upload images
-var myWidget = cloudinary.createUploadWidget({
-  cloudName: 'dwiyfuh6h',
-  uploadPreset: 'vjiganr9'
-}, (error, result) => {
-  if (!error && result && result.event === "success") {
-    console.log('Done! Here is the image info: ', result.info);
-  }
-}
-)
-// adding functionality to the button to perform upload on click
-document.getElementById("upload_widget").addEventListener("click", function () {
-  myWidget.open();
-}, false);
-
 // grabs values of inputs during signup and posts them to the server api route
 const signupHandler = async (event) => {
   event.preventDefault();
   const username = document.querySelector('#signup-username').value.trim();
   const email = document.querySelector('#signup-email').value.trim();
   const password = document.querySelector('#signup-password').value.trim();
+  const description = document.querySelector('#description').value.trim();
+
 
   if (username && email && password) {
     const response = await fetch('/api/users', {
       method: 'POST',
-      body: JSON.stringify({ username, email, password }),
+      body: JSON.stringify({ username, email, password, description }),
       headers: { 'Content-Type': 'application/json' },
     });
 
