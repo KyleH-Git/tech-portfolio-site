1baa38788b49e6bfc78b6852151881f9d0b2306d
NotSnowWhite <gflatch@att.net>
2024-05-10 13:14:20 -0500

    udpated description in profile and js

--- public/js/profile.js
+++ public/js/profile.js
@@ -12,3 +12,27 @@ var myWidget = cloudinary.createUploadWidget({
   document.getElementById("upload_widget").addEventListener("click", function () {
     myWidget.open();
   }, false);
+
+  // allow user to update description on click of edit button 
+  document.addEventListener('DOMContentLoaded', function() {
+    const descriptionDisplay = document.getElementById('description-display');
+    const editBtn = document.getElementById('edit-btn');
+
+    editBtn.addEventListener('click', function() {
+        const currentDescription = descriptionDisplay.textContent;
+        const descriptionInput = document.createElement('input');
+        descriptionInput.setAttribute('type', 'text');
+        descriptionInput.setAttribute('value', currentDescription);
+
+        descriptionDisplay.replaceWith(descriptionInput);
+
+        editBtn.textContent = 'Save';
+
+        editBtn.addEventListener('click', function() {
+            const newDescription = descriptionInput.value;
+            descriptionInput.replaceWith(descriptionDisplay);
+            descriptionDisplay.textContent = newDescription;
+            editBtn.textContent = 'Edit';
+        });
+    });
+});
